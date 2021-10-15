e139eedd7a887929aa2648ad19fdfbfc2ca5bd66
Leiza Girfatova <leiza-rad@yandex.ru>
2021-10-15 12:25:12 +0300

    first commit

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForUnregisteredAndUnderLimit() { //не зарегистрирован, сумма превышает лимита
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_000_60; //сумма
+        boolean registered = false;
+        long expected = 500; //ожидаемый
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldCalculateForUnregisteredAndOverLimit() { ////не зарегистрирован, сумма меньше лимита
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60; //сумма
+        boolean registered = false;
+        long expected = 10; //ожидаемый
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected,actual);
+    }
+}
