c91ef402a3b707f41b76d77c4d038cf8bf945a10
Leiza Girfatova <leiza-rad@yandex.ru>
2021-10-17 15:26:37 +0300

    commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -35,4 +35,36 @@ class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected,actual);
     }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() { //зарегистрирован, сумма превышает лимита
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_000_60; //сумма
+        boolean registered = true;
+        long expected = 500; //ожидаемый
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() { //зарегистрирован, сумма превышает лимита
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60; //сумма
+        boolean registered = true;
+        long expected = 30; //ожидаемый
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected,actual);
+    }
 }
